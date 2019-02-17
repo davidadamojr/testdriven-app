d547e3010a635380f1fb3a6e1ea5148711a4991e
David Adamo Jr <me@davidadamojr.com>
2019-02-17 13:32:23 -0500

    modified entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,13 @@
 
 echo "Waiting for postgres..."
 
-while ! nc -z users-db 5432; do
-   echo "Waiting for Postgres..."
+# while ! nc -z users-db 5432; do
+#    echo "Waiting for Postgres..."
+#    sleep 1
+# done
+
+while ! pg_isready -h users-db -p 5432 -q -U postgres; do
+   echo "Waiting for postgres - sleeping..." >&2
    sleep 1
 done
 

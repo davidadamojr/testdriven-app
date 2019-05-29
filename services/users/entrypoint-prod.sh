a9369dc813c4eab8e3c54e0ed85dd2dd7e6ad917
David Adamo Jr <me@davidadamojr.com>
2019-05-29 14:17:47 -0400

    updated files

--- services/users/entrypoint-prod.sh
+++ services/users/entrypoint-prod.sh
@@ -2,8 +2,13 @@
 
 echo "Waiting for postgres..."
 
-while ! nc -z users-db 5432; do
-    sleep 0.1
+# while ! nc -z users-db 5432; do
+#    sleep 0.1
+# done
+
+while ! pg_isready -h users-db -p 5432 -q -U postgres; do
+   echo "Waiting for postgres - sleeping..." >&2
+   sleep 2
 done
 
 echo "PostgreSQL started"

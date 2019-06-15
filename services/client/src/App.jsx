98ca6b054b2164de5bf2ec7e01eac3edc5727824
David Adamo Jr <me@davidadamojr.com>
2019-06-15 00:16:20 -0400

    updated components

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import UsersList from './components/UsersList';
-import AddUser from './components/AddUser';
 import About from './components/About';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
@@ -114,18 +113,7 @@ class App extends Component {
                                 <br />
                                 <Switch>
                                     <Route exact path='/' render={() => (
-                                        <div>
-                                            <h1 className="title is-1">All Users</h1>
-                                            <hr/><br/>
-                                            <AddUser
-                                                username={this.state.username}
-                                                email={this.state.email}
-                                                addUser={this.addUser}
-                                                handleChange={this.handleChange}
-                                            />
-                                            <br/><br/>
-                                            <UsersList users={this.state.users}/>
-                                        </div>
+                                        <UsersList users={this.state.users} />
                                     )} />
                                     <Route exact path='/about' component={About}/>
                                     <Route exact path='/register' render={() => (

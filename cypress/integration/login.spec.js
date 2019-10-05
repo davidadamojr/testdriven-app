4ca66c19e11dae002d9d30183048cea27a76137f
David Adamo Jr <me@davidadamojr.com>
2019-10-05 11:01:01 -0400

    react refactor

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -35,8 +35,13 @@ describe('Login', () => {
           .get('input[type="submit"]').click()
           .wait(100);
 
-        // assert user is redirected '/'
-        // assert '/' is displayed properly
+        // assert user is redirected to '/'
+        cy.get('.notification.is-success').contains('Welcome!');
+        cy.contains('Users').click();
+
+        // assert '/all-users' is displayed properly
+        cy.get('.navbar-burger').click();
+        cy.location().should((loc) => { expect(loc.pathname).to.eq('/all-users') });
         cy.contains('All Users');
         cy
           .get('table')
@@ -44,6 +49,7 @@ describe('Login', () => {
           .find('td').contains(username);
         cy.get('.notification.is-success').contains('Welcome!');
         cy.get('.navbar-burger').click();
+        cy.wait(300);
         cy.get('.navbar-menu').within(() => {
             cy
             .get('.navbar-item').contains('User Status')
@@ -53,11 +59,11 @@ describe('Login', () => {
         });
 
         // log a user out
-        cy.get('.navbar-burger').click();
         cy.get('a').contains('Log Out').click();
 
         // assert '/logout' is displayed properly
         cy.get('p').contains('You are now logged out');
+        cy.wait(300);
         cy.get('.navbar-menu').within(() => {
             cy
             .get('.navbar-item').contains('User Status').should('not.be.visible')
--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -9,6 +9,7 @@ import Form from './components/forms/Form';
 import Logout from './components/Logout';
 import UserStatus from './components/UserStatus';
 import Message from './components/Message';
+import Footer from './components/Footer';
 
 class App extends Component {
     constructor() {
@@ -90,7 +91,12 @@ class App extends Component {
                                 <br />
                                 <Switch>
                                     <Route exact path='/' render={() => (
-                                        <UsersList users={this.state.users} />
+                                        <p>Something</p>
+                                    )} />
+                                    <Route exact path='/all-users' render={() => (
+                                        <UsersList
+                                            users={this.state.users}
+                                        />
                                     )} />
                                     <Route exact path='/about' component={About}/>
                                     <Route exact path='/register' render={() => (
@@ -125,6 +131,7 @@ class App extends Component {
                         </div>
                     </div>
                 </section>
+                <Footer/>
             </div>
         )
     }

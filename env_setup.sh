cb42bc23cd06186bcfc88cf93aef6f21bfc8d9a7
David Adamo Jr <me@davidadamojr.com>
2019-10-24 01:28:41 -0400

    docker deploy staging

---
+++ env_setup.sh
@@ -0,0 +1,3 @@
+export REACT_APP_USERS_SERVICE_URL=http://localhost
+export REACT_APP_EXERCISES_SERVICE_URL=http://localhost
+export REACT_APP_API_GATEWAY_URL=https://aujxcds4kk.execute-api.us-east-1.amazonaws.com/v1/execute
